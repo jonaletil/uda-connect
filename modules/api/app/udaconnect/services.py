a9bf3d639ec6f4ab153264cab263aafaad791e4c
Josip <josip.naletilic@netformic.de>
2022-09-19 16:56:14 +0200

    cleanup

--- modules/api/app/udaconnect/services.py
+++ modules/api/app/udaconnect/services.py
@@ -1,12 +1,10 @@
 import logging
-from datetime import datetime, timedelta
-from typing import Dict, List
+from typing import Dict
 
 from app import db
 from app.udaconnect.models import Connection, Location, Person
 from app.udaconnect.schemas import ConnectionSchema, LocationSchema, PersonSchema
 from geoalchemy2.functions import ST_AsText, ST_Point
-from sqlalchemy.sql import text
 
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger("udaconnect-api")
@@ -17,8 +15,8 @@ class LocationService:
     def retrieve(location_id) -> Location:
         location, coord_text = (
             db.session.query(Location, Location.coordinate.ST_AsText())
-            .filter(Location.id == location_id)
-            .one()
+                .filter(Location.id == location_id)
+                .one()
         )
 
         # Rely on database to return text form of point to reduce overhead of conversion in app code
--- modules/connection-service/app/udaconnect/services.py
+++ modules/connection-service/app/udaconnect/services.py
@@ -16,10 +16,6 @@ class ConnectionService:
                       ) -> List[Connection]:
         """
         Finds all Person who have been within a given distance of a given Person within a date range.
-
-        This will run rather quickly locally, but this is an expensive method and will take a bit of time to run on
-        large datasets. This is by design: what are some ways or techniques to help make this data integrate more
-        smoothly for a better user experience for API consumers?
         """
         locations: List = db.session.query(Location).filter(
             Location.person_id == person_id
